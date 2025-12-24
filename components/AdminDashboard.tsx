311ddf65434d7b6ffce2ba1b5520e7422494ff7e
rgurses9 <rifatgurses@gmail.com>
2025-12-24 21:15:19 +0300

    Update AdminDashboard.tsx

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -1192,7 +1192,7 @@ const AdminDashboard: React.FC<AdminDashboardProps> = ({
                       Pasif Etkinlikler
                       {totalPassiveCount > 0 && (
                         <span className="ml-2 text-sm font-normal text-gray-500 dark:text-gray-400">
-                          ({totalPassiveCount} toplam - Son {passiveEvents.length} gösteriliyor)
+                          (Toplam {totalPassiveCount} etkinlik, son {passiveEvents.length} gösteriliyor)
                         </span>
                       )}
                     </h3>
